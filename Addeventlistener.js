b5136e327396b1431e3e641d1c362b8b7642643c
franco de la cruz <francoodlc@gmail.com>
2023-04-16 15:34:14 -0300

    Agregar .gitignore y primeros archivos del proyecto

---
+++ Addeventlistener.js
@@ -0,0 +1,15 @@
+const input1 = document.querySelector("#numero1"); 
+const form = document.querySelector("#form"); 
+const input2 = document.querySelector("#numero2"); 
+const btn = document.querySelector("#btncalcular"); 
+const suma = document.querySelector("#sumaid");
+
+//addEventListener
+
+form.addEventListener("mouseover", enviarFormulario)
+
+function enviarFormulario(e){
+    e.preventDefault();
+
+    alert("virus")
+}
---
+++ script.js
@@ -0,0 +1,14 @@
+const input1 = document.querySelector("#numero1"); 
+const form = document.querySelector("#form"); 
+const input2 = document.querySelector("#numero2"); 
+const btn = document.querySelector("#btncalcular"); 
+const suma = document.querySelector("#sumaid");
+
+//addEventListener
+
+form.addEventListener("mousedown", getEvtType)
+
+function  getEvtType(evt) {
+    console.group("JAJAJA esto es una farsa");
+}
+   
